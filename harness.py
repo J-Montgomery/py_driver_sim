5b6b0e5261175ae83ff534328920be16bce3993d
Jordan M <montytyper@msn.com>
2022-03-05 20:03:14 -0800

    Add probing.

--- harness.py
+++ harness.py
@@ -26,12 +26,13 @@ for file in header_list:
 code = "".join(source)
 ffibuilder.embedding_api(code)
 
+# Need to fix this, this is gross
 ffibuilder.set_source(
     "model",
     r"""
     #include "lib.h"
 
-#define SPI_NAME_SIZE	32
+    #define SPI_NAME_SIZE	32
 #define SPI_MODULE_PREFIX "spi:"
 
 struct device {
@@ -66,9 +67,50 @@ struct spi_driver {
 	struct device_driver	driver;
 };
 
-
+int call_probe(struct spi_driver *sdrv) {
+    return sdrv->probe(0);
+}
 """,
 )
+
+ffibuilder.cdef(r"""
+#define SPI_NAME_SIZE	32
+
+struct device {
+	struct device	*parent;
+	const char	*init_name;
+};
+
+struct device_driver {
+	const char		*name;
+};
+
+struct spi_device_id {
+char name[SPI_NAME_SIZE];
+unsigned long driver_data;
+};
+
+    struct spi_controller {
+	struct device	dev;
+};
+
+struct spi_device {
+	struct device		dev;
+	struct spi_controller	*controller;
+	struct spi_controller	*master;	/* compatibility layer */
+};
+
+struct spi_driver {
+	const struct spi_device_id *id_table;
+	int			(*probe)(struct spi_device *spi);
+	int			(*remove)(struct spi_device *spi);
+	void			(*shutdown)(struct spi_device *spi);
+	struct device_driver	driver;
+};
+
+int call_probe(struct spi_driver *sdrv);
+""")
+
 source = []
 
 for file in module_list:
--- model.py
+++ model.py
@@ -1,10 +1,35 @@
-from model import ffi
+from model import ffi, lib
 
+
+from inspect import getmembers
 
 def get_string(cdata):
     return ffi.string(cdata).decode('ascii')
 
-device_table = []
+def cdata_dict(cd):
+    if isinstance(cd, ffi.CData):
+        try:
+            return ffi.string(cd)
+        except TypeError:
+            try:
+                return [cdata_dict(x) for x in cd]
+            except TypeError:
+                return {k: cdata_dict(v) for k, v in getmembers(cd)}
+    else:
+        return cd
+
+device_table = dict()
+
+driver_table = dict()
+
+def find_device_table(entry):
+    name = entry['name'].decode('ascii')
+    for driver in device_table:
+        devices = device_table[driver]
+        for dev in devices:
+            if name in dev:
+                print("found!", entry, dev, driver)
+                return driver
 
 @ffi.def_extern()
 def call_stub(x, y):
@@ -13,22 +38,36 @@ def call_stub(x, y):
 
 @ffi.def_extern()
 def spi_register_driver(sdrv):
-    print("register_driver", sdrv)
+    driver = cdata_dict(sdrv[0])
+    print("register_driver", sdrv, driver['id_table'])
+    drv = find_device_table(driver['id_table'])
+    driver_table[drv] = sdrv
     return 0
 
 @ffi.def_extern()
 def spi_unregister_driver(sdrv):
     print("unregister_driver", sdrv)
 
-# initialize_device_table(char *type, char *name, char *device_name, int device_id);
 @ffi.def_extern()
 def initialize_device_table(type, name, dev_name, dev_id):
-    dev_entry = [get_string(type), get_string(name), get_string(dev_name), dev_id]
-    print("Init device table: {}".format(dev_entry))
-    device_table.append(dev_entry)
+    key = get_string(name)
+    dev_entry = [get_string(dev_name), dev_id]
+    if key in device_table:
+        device_table[key].append(dev_entry)
+    else:
+        device_table[key] = [dev_entry]
+    print("Init device table ({} : {}".format(key, dev_entry))
+    print(device_table)
 
 
 @ffi.def_extern()
 def main(argv, argc):
     print("Hello, World!")
+    if not len(driver_table):
+        print("No drivers found")
+    else:
+        drivers = [driver_table[x] for x in driver_table]
+        print("driver ", drivers)
+        # attempt to probe the first driver
+        lib.call_probe(drivers[0])
     return 0
